176c8b000bd73b6bc72b68610789d75cf9f15d95
Gonzalo Santos Orellana <gsantosorellana.dev@gmail.com>
2023-08-22 02:33:43 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import { fuels, yearsOfProduction } from '@/constants'
 import { fetchCars } from '@/utils'
+import { HomeProps } from '@/types'
 
-export default async function Home({ searchParams }) {
+export default async function Home({ searchParams }: HomeProps) {
 	const allCars = await fetchCars({
 		manufacturer: searchParams.manufacturer || '',
 		year: searchParams.year || 2022,
